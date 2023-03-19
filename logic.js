46ccb8463298488ffa718979d6e7d3aaabba3c75
SalaniLeo <leonardo07.salani@gmail.com>
2023-03-19 15:43:16 +0100

    update

--- logic.js
+++ logic.js
@@ -3,6 +3,10 @@ var screen = document.getElementById("container")
 var screenRect = document.body.getBoundingClientRect();
 var playerRect = screen.getBoundingClientRect();
 
+let element1 = document.getElementById("whoami")
+let element2 = document.getElementById("skills")
+let element3 = document.getElementById("projects")
+
 function getCursor(event) {
     let x = event.clientX;
     let y = event.clientY;
@@ -15,41 +19,51 @@ function getCursor(event) {
 
 }
 
-let element1 = document.getElementById("whoami")
-let element2 = document.getElementById("skills")
-let element3 = document.getElementById("projects")
+if (!/Android|iPhone/i.test(navigator.userAgent)) {
+
+  element1.classList.add("draggable");
+  element2.classList.add("draggable");
+  element3.classList.add("draggable");
+
+}
+
+if(element1.classList.contains("draggable")&&element2.classList.contains("draggable")&&element3.classList.contains("draggable")) {
+
+moveItem(element1)
+moveItem(element2)
+moveItem(element3)
+
+}
+
+function moveItem(item){
 
 
+  item.onmousedown = function(event) {
 
-
-
-
-element1.onmousedown = function(event) {
-
-    let shiftX = event.clientX - element1.getBoundingClientRect().left;
-    let shiftY = event.clientY - element1.getBoundingClientRect().top;
+    let shiftX = event.clientX - item.getBoundingClientRect().left;
+    let shiftY = event.clientY - item.getBoundingClientRect().top;
   
-    element1.style.position = 'absolute';
-    element1.style.transitionDuration = '0s';
-    element1.style.zIndex = 1000;
-    document.body.append(element1);
+    item.style.position = 'fixed';
+    item.style.transitionDuration = '0s';
+    document.body.append(item);
     moveAt(event.pageX, event.pageY);
 
     function moveAt(pageX, pageY) {
-        
-        element1.style.left = pageX+27.5 - shiftX + 'px';
-        element1.style.top = pageY+12.5- shiftY + 'px';
-      
+        if(screenRect.width>pageX&&0<pageX){
+          item.style.left = pageX+27.5 - shiftX + 'px';
+          item.style.top = pageY+12.5- shiftY + 'px';
+        }
+
     }
     function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
     
     }
     document.addEventListener('mousemove', onMouseMove);
-    element1.onmouseup = function() {
+    item.onmouseup = function() {
       document.removeEventListener('mousemove', onMouseMove);
-      element1.style.transitionDuration = '0.25s';
-      element1.onmouseup = null;
+      item.style.transitionDuration = '0.25s';
+      item.onmouseup = null;
     };
   };
   
@@ -57,62 +71,5 @@ element1.onmousedown = function(event) {
     return false;
   };
 
-element2.onmousedown = function(event) {
 
-    let shiftX = event.clientX - element2.getBoundingClientRect().left;
-    let shiftY = event.clientY - element2.getBoundingClientRect().top;
-  
-    element2.style.position = 'absolute';
-    element2.style.transitionDuration = '0s';
-    element2.style.zIndex = 1000;
-    document.body.append(element2);
-    moveAt(event.pageX, event.pageY);
-    function moveAt(pageX, pageY) {
-        element2.style.left = pageX+27.5 - shiftX + 'px';
-        element2.style.top = pageY+12.5- shiftY + 'px';
-    }
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-    }
-    document.addEventListener('mousemove', onMouseMove);
-    element2.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      element2.style.transitionDuration = '0.25s';
-      element2.onmouseup = null;
-    };
-  };
-  
-  element2.ondragstart = function() {
-    return false;
-  };
-
-
-element3.onmousedown = function(event) {
-  
-      let shiftX = event.clientX - element3.getBoundingClientRect().left;
-      let shiftY = event.clientY - element3.getBoundingClientRect().top;
-    
-      element3.style.position = 'absolute';
-      element3.style.transitionDuration = '0s';
-      element3.style.zIndex = 1000;
-      document.body.append(element3);
-      moveAt(event.pageX, event.pageY);
-      function moveAt(pageX, pageY) {
-        element3.style.left = pageX+27.5 - shiftX + 'px';
-        element3.style.top = pageY+12.5- shiftY + 'px';
-      }
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-        console.log(event.pageX, event.pageY)
-      }
-      document.addEventListener('mousemove', onMouseMove);
-      element3.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        element3.style.transitionDuration = '0.25s';
-        element3.onmouseup = null;
-      };
-    };
-    
-    element3.ondragstart = function() {
-        return false;
-    };
+}
