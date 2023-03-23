b6d36357fba8c1fe29186f50ade6295b5f24ddeb
SalaniLeo <leonardo07.salani@gmail.com>
2023-03-23 23:13:03 +0100

    update

--- background.js
+++ background.js
@@ -1,69 +1,69 @@
 const Star = function(x, y, z) {
 
-    this.x = x; this.y = y; this.z = z;
+  this.x = x; this.y = y; this.z = z;
 
-    this.size = 1;
+  this.size = 3;
 
-  };
+};
 
-  var context = document.querySelector("canvas").getContext("2d");
+var context = document.querySelector("canvas").getContext("2d");
 
-  var height = document.documentElement.clientHeight;
-  var width  = document.documentElement.clientWidth;
+var height = document.documentElement.clientHeight;
+var width  = document.documentElement.clientWidth;
 
-  var stars = new Array();
+var stars = new Array();
 
-  var max_depth = 100;
+var max_depth = 100000;
 
-  for(let index = 0; index < 20; index ++) stars[index] = new Star(Math.random() * width, Math.random() * height, index * (max_depth / 200));
+for(let index = 0; index < 200; index ++) stars[index] = new Star(Math.random() * width, Math.random() * height, index * (max_depth / 200));
 
-  function loop() {
+function loop() {
 
-    window.requestAnimationFrame(loop);
+  window.requestAnimationFrame(loop);
 
-    height = document.documentElement.clientHeight;
-    width  = document.documentElement.clientWidth;
+  height = document.documentElement.clientHeight;
+  width  = document.documentElement.clientWidth;
 
-    context.canvas.height = height;
-    context.canvas.width  = width;
+  context.canvas.height = height;
+  context.canvas.width  = width;
 
-    context.fillStyle = "#000000";
-    context.fillRect(0, 0, width, height);
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, width, height);
 
-    for (let index = stars.length - 1; index > -1; index --) {
+  for (let index = stars.length - 1; index > -1; index --) {
 
-      let star = stars[index];
+    let star = stars[index];
 
-      star.z -= 5;
+    star.z -= 5;
 
-      if (star.z < 0) {
+    if (star.z < 0) {
 
-        stars.push(stars.splice(index, 1)[0]);
-        star.z = max_depth;
-        continue;
-
-      }
-
-      let translate_x = width * 0.5;
-      let translate_y = height * 0.5;
-
-      let field_of_view = (height + width) * 8;
-
-      let star_x = (star.x - translate_x) / (star.z / field_of_view) + translate_x;
-      let star_y = (star.y - translate_y) / (star.z / field_of_view) + translate_y;
-
-      let scale = field_of_view / (field_of_view + star.z);
-
-      let color = Math.floor(scale * 256);
-
-      context.fillStyle = "rgb(" + color + "," + color + "," + color + ")";
-      context.fillRect(star_x, star_y, star.size * scale, star.size * scale);
+      stars.push(stars.splice(index, 1)[0]);
+      star.z = max_depth;
+      continue;
 
     }
 
+    let translate_x = width * 0.5;
+    let translate_y = height * 0.5;
+
+    let field_of_view = (height + width) * 8;
+
+    let star_x = (star.x - translate_x) / (star.z / field_of_view) + translate_x;
+    let star_y = (star.y - translate_y) / (star.z / field_of_view) + translate_y;
+
+    let scale = field_of_view / (field_of_view + star.z);
+
+    let color = Math.floor(scale * 256);
+
+    context.fillStyle = "rgb(" + color + "," + color + "," + color + ")";
+    context.fillRect(star_x, star_y, star.size * scale, star.size * scale);
+
   }
-  function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
-  }
-  loop();
+
+}
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+}
+loop();
--- logic.js
+++ logic.js
@@ -1,11 +1,65 @@
 var cards = document.getElementById("cards")
-var cardsRect = cards.getBoundingClientRect();
+var next = document.getElementById("next")
+
+var stage = 0
+let lastKnownScrollPosition = 0;
+let ticking = false;
+var secondPage = false
+var secondPagePos = 200
+
+function doSomething(scrollPos) {
+
+    // if(scrollPos>secondPagePos&&secondPage==false){
+
+    //     document.getElementById("secondPage").scrollIntoView();
+    //     secondPage = true
+    // }    
+
+    // if(scrollPos<secondPagePos&&secondPage==true){
+    //     secondPage = false
+
+    // }
+    
+}
+
+document.addEventListener("scroll", (event) => {
+  lastKnownScrollPosition = window.scrollY;
+
+//   if(window.scrollY>lastKnownScrollPosition){
+
+//   }
+
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
 
 
 function nextStage(){
 
-    cards.style.transitionDuration = '2s'
-    cards.style.top = cardsRect.top + 100 + 'px';
-    cards.classList.add('hide')
+    if(stage==0){
+
+        setTimeout(() => {
+            var next = document.getElementById("div1")
+            next.classList.add("show")
+        },0);
+        setTimeout(() => {
+            var next = document.getElementById("div2")
+            next.classList.add("show")
+        },200);
+        setTimeout(() => {
+            var next = document.getElementById("secondPage")
+            next.classList.add("show")
+        },400);
+
+        stage++
+
+    }
 
 }
