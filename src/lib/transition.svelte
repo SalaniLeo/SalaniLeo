0bdd8fd8500fea8ff9b23131a6492faf462d19b5
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2024-03-17 22:53:14 +0100

    🖌️ Standardized css, modified transition animation

--- src/lib/transition.svelte
+++ src/lib/transition.svelte
@@ -5,8 +5,8 @@
 
 {#key pathname}
   <div
-    in:fly={{ x: -10, duration: 500, delay: 500 }}
-    out:fly={{ x: 5, duration: 500 }}
+    in:fly={{ x: 25, duration: 500, delay: 500 }}
+    out:fly={{ x: 25, duration: 500 }}
   >
     <slot />
   </div>
