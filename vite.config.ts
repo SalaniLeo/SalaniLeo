9b27a0951f235120c152f1c0373acd92b35f021b
salanileo <leonardo07.salani@gmail.com>
2024-06-08 11:42:43 +0200

    🔧 Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 5500
-	  },
-	  preview: {
-		port: 8080
-	  }
+	plugins: [sveltekit()]
 });
